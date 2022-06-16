1188936c43bc539009a2e2f478310f738f69f3ff
AliyaAM <aliya.amirova005@gmail.com>
2022-06-16 21:12:10 +0100

    removed scale from glm

--- wealth_gradient_function.R
+++ wealth_gradient_function.R
@@ -103,7 +103,7 @@ wealth_gradient_function = function (data_ELSA,
     
     
     #wealth gradient 
-    wealth_discrimination =  summary(glm(discrimination ~ scale(wealth), family = "binomial", data = data_both_countries))
+    wealth_discrimination =  summary(glm(discrimination ~ wealth, family = "binomial", data = data_both_countries))
     wealth_gradient = wealth_discrimination$coefficients
     wealth_gradient = as.data.frame(wealth_gradient)
     write.csv(wealth_gradient, file = paste(OUTPUT_ROOT, folder,  "wealth_gradient_discrimination.csv", sep=""))
@@ -118,7 +118,7 @@ wealth_gradient_function = function (data_ELSA,
     ELSA_subset_wealth_gradient = subset(data_both_countries, data_both_countries$country_cat == 1)
     
     
-    wealth_discrimination_ELSA =  summary(glm(discrimination ~ scale(wealth), family = "binomial", data = ELSA_subset_wealth_gradient))
+    wealth_discrimination_ELSA =  summary(glm(discrimination ~ wealth, family = "binomial", data = ELSA_subset_wealth_gradient))
     wealth_gradient_ELSA = wealth_discrimination_ELSA$coefficients
     wealth_gradient_ELSA = as.data.frame(wealth_gradient_ELSA)
     
@@ -133,7 +133,7 @@ wealth_gradient_function = function (data_ELSA,
     HRS_subset_wealth_gradient = subset(data_both_countries, data_both_countries$country_cat == 0)
     
     
-    wealth_discrimination_HRS =  summary(glm(discrimination ~ scale(wealth), family = "binomial", data = HRS_subset_wealth_gradient))
+    wealth_discrimination_HRS =  summary(glm(discrimination ~ wealth, family = "binomial", data = HRS_subset_wealth_gradient))
     wealth_gradient_HRS = wealth_discrimination_HRS$coefficients
     wealth_gradient_HRS = as.data.frame(wealth_gradient_HRS)
     
@@ -156,7 +156,7 @@ wealth_gradient_function = function (data_ELSA,
     
     
     #wealth gradient 
-    wealth_discrimination_adjusted =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]), 
+    wealth_discrimination_adjusted =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1], 
                                                   family = "binomial", data = data_both_countries))
     wealth_gradient_adjusted  = wealth_discrimination_adjusted$coefficients
     wealth_gradient_adjusted = as.data.frame(wealth_gradient_adjusted)
@@ -174,7 +174,7 @@ wealth_gradient_function = function (data_ELSA,
     ELSA_subset_wealth_gradient = subset(data_both_countries, data_both_countries$country_cat == 1)
     
     
-    wealth_discrimination_ELSA_adjusted =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]), 
+    wealth_discrimination_ELSA_adjusted =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1], 
                                                        family = "binomial", data = ELSA_subset_wealth_gradient))
     wealth_gradient_ELSA_adjusted = wealth_discrimination_ELSA_adjusted$coefficients
     wealth_gradient_ELSA_adjusted = as.data.frame(wealth_gradient_ELSA_adjusted)
@@ -191,7 +191,7 @@ wealth_gradient_function = function (data_ELSA,
     HRS_subset_wealth_gradient = subset(data_both_countries, data_both_countries$country_cat == 0)
     
     
-    wealth_discrimination_HRS_adjusted =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]), 
+    wealth_discrimination_HRS_adjusted =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1], 
                                                       family = "binomial", data = HRS_subset_wealth_gradient))
     wealth_gradient_HRS_adjusted = wealth_discrimination_HRS_adjusted$coefficients
     wealth_gradient_HRS_adjusted = as.data.frame(wealth_gradient_HRS_adjusted)
@@ -220,7 +220,7 @@ wealth_gradient_function = function (data_ELSA,
                                                        data_HRS_subset[ ,   wealth_gradient_cov2])
     
     #wealth gradient 
-    wealth_discrimination_adjusted2 =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]) + scale(data_both_countries[ ,   wealth_gradient_cov2]), 
+    wealth_discrimination_adjusted2 =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1] + data_both_countries[ ,   wealth_gradient_cov2], 
                                                    family = "binomial", data = data_both_countries))
     
     wealth_gradient_adjusted2  = wealth_discrimination_adjusted2$coefficients
@@ -238,7 +238,7 @@ wealth_gradient_function = function (data_ELSA,
     ELSA_subset_wealth_gradient = subset(data_both_countries, data_both_countries$country_cat == 1)
     
     
-    wealth_discrimination_ELSA_adjusted2 =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]) + scale(data_both_countries[ ,   wealth_gradient_cov2]), 
+    wealth_discrimination_ELSA_adjusted2 =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1] + data_both_countries[ ,   wealth_gradient_cov2], 
                                                         family = "binomial", data = ELSA_subset_wealth_gradient))
     
     wealth_gradient_ELSA_adjusted2 = wealth_discrimination_ELSA_adjusted2$coefficients
@@ -255,7 +255,7 @@ wealth_gradient_function = function (data_ELSA,
     HRS_subset_wealth_gradient = subset(data_both_countries, data_both_countries$country_cat == 0)
     
     
-    wealth_discrimination_HRS_adjusted2 =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]) + scale(data_both_countries[ ,   wealth_gradient_cov2]), 
+    wealth_discrimination_HRS_adjusted2 =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1] + data_both_countries[ ,   wealth_gradient_cov2], 
                                                        family = "binomial", data = HRS_subset_wealth_gradient))
     
     wealth_gradient_HRS_adjusted2 = wealth_discrimination_HRS_adjusted2$coefficients
@@ -286,7 +286,7 @@ wealth_gradient_function = function (data_ELSA,
     
     
     #wealth gradient 
-    wealth_discrimination_adjusted3 =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]) + scale(data_both_countries[ ,   wealth_gradient_cov2]) + scale(data_both_countries[ ,   wealth_gradient_cov3]), 
+    wealth_discrimination_adjusted3 =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1] + data_both_countries[ ,   wealth_gradient_cov2] + data_both_countries[ ,   wealth_gradient_cov3], 
                                                    family = "binomial", data = data_both_countries))
     
     wealth_gradient_adjusted3  = wealth_discrimination_adjusted3$coefficients
@@ -303,7 +303,7 @@ wealth_gradient_function = function (data_ELSA,
     ELSA_subset_wealth_gradient = subset(data_both_countries, data_both_countries$country_cat == 1)
     
     
-    wealth_discrimination_ELSA_adjusted3 =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]) + scale(data_both_countries[ ,   wealth_gradient_cov2]) + scale(data_both_countries[ ,   wealth_gradient_cov3]), 
+    wealth_discrimination_ELSA_adjusted3 =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1]) + data_both_countries[ ,   wealth_gradient_cov2] + data_both_countries[ ,   wealth_gradient_cov3], 
                                                         family = "binomial", data = ELSA_subset_wealth_gradient))
     
     wealth_gradient_ELSA_adjusted3 = wealth_discrimination_ELSA_adjusted3$coefficients
@@ -320,7 +320,7 @@ wealth_gradient_function = function (data_ELSA,
     HRS_subset_wealth_gradient = subset(data_both_countries, data_both_countries$country_cat == 0)
     
     
-    wealth_discrimination_HRS_adjusted3 =  summary(glm(discrimination ~ scale(wealth) + scale(data_both_countries[ ,   wealth_gradient_cov1]) + scale(data_both_countries[ ,   wealth_gradient_cov2]) + scale(data_both_countries[ ,   wealth_gradient_cov3]),
+    wealth_discrimination_HRS_adjusted3 =  summary(glm(discrimination ~ wealth + data_both_countries[ ,   wealth_gradient_cov1] + data_both_countries[ ,   wealth_gradient_cov2] + data_both_countries[ ,   wealth_gradient_cov3],
                                                        family = "binomial", data = HRS_subset_wealth_gradient))
     
     wealth_gradient_HRS_adjusted3 = wealth_discrimination_HRS_adjusted3$coefficients
