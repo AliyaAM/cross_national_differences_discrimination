f06a02eac0dd04a6a5d4f90c6fc412e5407b5a16
AliyaAM <aliya.amirova005@gmail.com>
2022-06-19 00:12:47 +0100

    Update subsetting_sample_char.R

--- subsetting_sample_char.R
+++ subsetting_sample_char.R
@@ -50,21 +50,28 @@ subsetting_sample_char = function (data_ELSA,
   N_HRS_subset = nrow(data_HRS_subset)
   
   
-  #calculate the number of people who perceived this type of discrimination 
-  
-  #ELSA_discrimYES_subset = subset(data_ELSA_subset, data_ELSA_subset[ , discrimination_VAR_elsa] == 1) 
-  #HRS_discrimYES_subset = subset(data_HRS_subset,  data_HRS_subset[ , discrimination_VAR_hrs] == 1)
-  
-  #N_ELSA_discrimYES = nrow(ELSA_discrimYES_subset)
-  #N_HRS_discrimYES = nrow(HRS_discrimYES_subset)
-  
-  #predictor dummy varibale: country (UK vs USA)
   country_cat = c(data_ELSA_subset$country, 
                   data_HRS_subset$country)
   
   country_cat = as.factor(country_cat)
   
   data_both_countries = data.frame(country_cat)
+  
+  print("ELSA n: ")
+  print(nrow(data_ELSA_subset)) 
+  
+  
+  print("HRS n: ")
+  print(nrow(data_HRS_subset)) 
+  
+  print("both added")
+  both_n = nrow(data_ELSA_subset) + nrow(data_HRS_subset)
+  print(both_n)
+  
+  print("data_both_countries n: ")
+  print(nrow(data_both_countries)) 
+  
+  
   #outcome concatinated into a new dataframe pooling ELSA and HRS (make sure the order as above)
   
   #data_both_countries$discrimination = c(data_ELSA_subset[ , discrimination_VAR_elsa],
@@ -72,10 +79,12 @@ subsetting_sample_char = function (data_ELSA,
   
   #data_both_countries$discrimination = as.factor(data_both_countries$discrimination)
   
-
+  
   
   data_both_countries$age =  c(data_ELSA_subset$age,
                                data_HRS_subset$age)
+  
+  
   
   
   data_both_countries$sex =  c(data_ELSA_subset$sex,
@@ -113,18 +122,22 @@ subsetting_sample_char = function (data_ELSA,
   version = version
   subset_name = subset_name
   
-  folder = paste(path, subset_name, version, "/", sep = "")
+  folder = paste(subset_name, version, "/", sep = "")
   
-  #dir.create(paste(path, folder, sep = ""))
+  dir.create(paste(path, folder, sep = ""))
   
-  pdf(file = paste(folder, "/age_compare_plot.pdf"),   # The directory you want to save the file in
+  #pdf(file = paste(folder, "age_compare_plot.pdf", sep = ""),   # The directory you want to save the file in
+  #    width = 4, # The width of the plot in inches
+  #    height = 4)
+  
+  pdf(file = paste(path, folder, "age_compare_plot.pdf", sep = ""),   # The directory you want to save the file in
       width = 4, # The width of the plot in inches
       height = 4)
   
   #plots for age
   age_plot = ggboxplot(data_both_countries, x = "country_cat", y = "age", 
-            color = "country_cat", palette = c("#00AFBB", "#E7B800"),
-            ylab = "age", xlab = "country_cat")
+                       color = "country_cat", palette = c("#00AFBB", "#E7B800"),
+                       ylab = "age", xlab = "country_cat")
   
   
   print(age_plot)
@@ -132,123 +145,160 @@ subsetting_sample_char = function (data_ELSA,
   dev.off()
   
   
-  #testing assumption of normality test 
-  # Shapiro-Wilk normality test 
-  SW_test_1 = with(data_both_countries, shapiro.test(age[country_cat == 0]))# should be >0.05
-  # Shapiro-Wilk normality test 
-  SW_test_1 = with(data_both_countries, shapiro.test(age[country_cat == 1])) # should be >0.05
-  
-  SW_test_1 = as.data.frame(SW_test_1)
-  SW_test_2 = as.data.frame(SW_test_2)
-  
-  write.csv(SW_test_1, file = paste(folder, "/age_SW_test_1.csv")) 
-  
-  write.csv(SW_test_2, file = paste(folder, "/age_SW_test_2.csv")) 
   
   
   #teting that US sample and UK sample have the same variance 
   res.ftest_age <- var.test(age ~ country_cat, data = data_both_countries)
   res.ftest_age
   
-  write.csv(res.ftest_age, file = paste(folder, "/age_ftest.csv")) 
+  ftest_names = c("F value", 
+                  "95% CI ", 
+                  "95% CI", 
+                  "p-value")
+  
+  age_ftest_age = c(res.ftest_age$statistic, 
+                    res.ftest_age$conf.int[1], 
+                    res.ftest_age$conf.int[2], 
+                    res.ftest_age$p.value) 
+  
+  age_ftest_age = rbind(ftest_names, age_ftest_age)
+  age_ftest_age = as.data.frame(age_ftest_age)
+  
+  write.csv(age_ftest_age, file = paste(path, folder, "age_ftest.csv", sep = "")) 
   
   
   #t-test comparing differences in age between US sample and UK sample 
   res_age <- t.test(age ~ country_cat, data = data_both_countries, var.equal = TRUE)
-  res_age
   
-  write.csv(res_age, file = paste(folder, "/age_compare.csv")) 
+  T_test_names = c("t value", 
+                   "df",
+                   "p-value", 
+                   "95% CI ", 
+                   "95% CI",
+                   "samples means")
   
-#############
+  res_age = c(res_age$statistic, 
+              res_age$parameter, 
+              res_age$p.value, 
+              res_age$conf.int[1], 
+              res_age$conf.int[2], 
+              res_age$estimate)
+  
+  res_age = rbind(T_test_names, res_age)
+  
+  res_age = as.data.frame(res_age)
+  
+  write.csv(res_age, file = paste(path, folder, "age_compare.csv", sep = "")) 
+  
+  #############
   
   
   #plots for wealth
   
   
-  pdf(file = paste(folder, "/wealth_compare_plot.pdf"),   # The directory you want to save the file in
+  pdf(file = paste(path, folder, "wealth_compare_plot.pdf", sep = ""),   # The directory you want to save the file in
       width = 4, # The width of the plot in inches
       height = 4)
   
- wealth_plot =  ggboxplot(data_both_countries, x = "country_cat", y = "wealth", 
-            color = "country_cat", palette = c("#00AFBB", "#E7B800"),
-            ylab = "wealth", xlab = "country_cat")
+  wealth_plot =  ggboxplot(data_both_countries, x = "country_cat", y = "wealth", 
+                           color = "country_cat", palette = c("#00AFBB", "#E7B800"),
+                           ylab = "wealth", xlab = "country_cat")
   
   print(wealth_plot)
   
   dev.off()
   
-  #testing assumption of normality test 
-  # Shapiro-Wilk normality test 
-  SW_test_1 = with(data_both_countries, shapiro.test(wealth[country_cat == 0]))# should be >0.05
-  # Shapiro-Wilk normality test 
-  SW_test_1 = with(data_both_countries, shapiro.test(wealth[country_cat == 1])) # should be >0.05
   
-  SW_test_1 = as.data.frame(SW_test_1)
-  SW_test_2 = as.data.frame(SW_test_2)
   
-  write.csv(SW_test_1, file = paste(folder, "/wealth_SW_test_1.csv")) 
-  
-  write.csv(SW_test_2, file = paste(folder, "/wealth_SW_test_2.csv")) 
   
   
   #teting that US sample and UK sample have the same variance 
   res.ftest_wealth <- var.test(wealth ~ country_cat, data = data_both_countries)
   res.ftest_wealth
   
-  write.csv(res.ftest_wealth, file = paste(folder, "/wealth_ftest.csv")) 
+  res.ftest_wealth = c(res.ftest_wealth$statistic, 
+                       res.ftest_wealth$conf.int[1], 
+                       res.ftest_wealth$conf.int[2], 
+                       res.ftest_wealth$p.value) 
+  
+  res.ftest_wealth = rbind(ftest_names, res.ftest_wealth)
+  res.ftest_wealth = as.data.frame(res.ftest_wealth)
+  
+  write.csv(res.ftest_wealth, file = paste(path, folder, "wealth_ftest.csv", sep = "")) 
   
   
   #t-test comparing differences in wealth between US sample and UK sample 
   res_wealth <- t.test(wealth ~ country_cat, data = data_both_countries, var.equal = TRUE)
   res_wealth
   
-  write.csv(res_wealth, file = paste(folder, "/wealth_compare.csv")) 
+  res_wealth = c(res_wealth$statistic, 
+                 res_wealth$parameter, 
+                 res_wealth$p.value, 
+                 res_wealth$conf.int[1], 
+                 res_wealth$conf.int[2], 
+                 res_wealth$estimate)
+  
+  res_wealth = rbind(T_test_names, res_wealth)
+  
+  
+  res_wealth = as.data.frame(res_wealth)
+  
+  write.csv(res_wealth, file = paste(path, folder, "wealth_compare.csv",  sep = "")) 
   
   
   #plots for BMI
   
   
-  pdf(file = paste(folder, "/BMI_compare_plot.pdf"),   # The directory you want to save the file in
+  pdf(file = paste(path, folder, "BMI_compare_plot.pdf", sep = ""),   # The directory you want to save the file in
       width = 4, # The width of the plot in inches
       height = 4)
   
   
   
   BMI_plot =  ggboxplot(data_both_countries, x = "country_cat", y = "BMI", 
-                           color = "country_cat", palette = c("#00AFBB", "#E7B800"),
-                           ylab = "BMI", xlab = "country_cat")
+                        color = "country_cat", palette = c("#00AFBB", "#E7B800"),
+                        ylab = "BMI", xlab = "country_cat")
   
   print(BMI_plot)
   
   dev.off()
   
   
-  #testing assumption of normality test 
-  # Shapiro-Wilk normality test 
-  SW_test_1 = with(data_both_countries, shapiro.test(BMI[country_cat == 0]))# should be >0.05
-  # Shapiro-Wilk normality test 
-  SW_test_1 = with(data_both_countries, shapiro.test(BMI[country_cat == 1])) # should be >0.05
   
-  SW_test_1 = as.data.frame(SW_test_1)
-  SW_test_2 = as.data.frame(SW_test_2)
-  
-  write.csv(SW_test_1, file = paste(folder, "/BMI_SW_test_1.csv")) 
-            
-  write.csv(SW_test_2, file = paste(folder, "/BMI_SW_test_2.csv")) 
-                      
-            
   #teting that US sample and UK sample have the same variance 
   res.ftest_BMI <- var.test(BMI ~ country_cat, data = data_both_countries)
   res.ftest_BMI
   
-  write.csv(res.ftest_BMI, file = paste(folder, "/BMI_ftest.csv")) 
-            
+  res.ftest_BMI = c(res.ftest_BMI$statistic, 
+                    res.ftest_BMI$conf.int[1], 
+                    res.ftest_BMI$conf.int[2], 
+                    res.ftest_BMI$p.value) 
+  
+  res.ftest_BMI = rbind(ftest_names, res.ftest_BMI)
+  res.ftest_BMI = as.data.frame(res.ftest_BMI)
+  
+  write.csv(res.ftest_BMI, file = paste(path, folder, "BMI_ftest.csv", sep = "")) 
+  
   
   #t-test comparing differences in BMI between US sample and UK sample 
   res_BMI <- t.test(BMI ~ country_cat, data = data_both_countries, var.equal = TRUE)
   res_BMI
   
-  write.csv(res_BMI, file = paste(folder, "/BMI_compare.csv")) 
+  
+  res_BMI = c(res_BMI$statistic, 
+              res_BMI$parameter, 
+              res_BMI$p.value, 
+              res_BMI$conf.int[1], 
+              res_BMI$conf.int[2], 
+              res_BMI$estimate)
+  
+  res_BMI = rbind(T_test_names, res_BMI)
+  
+  
+  res_BMI = as.data.frame(res_BMI)
+  
+  write.csv(res_BMI, file = paste(path, folder, "BMI_compare.csv", sep = "")) 
+  
   
  
 }
